20fafdee759ffc1c53ab37b73e1961d6e7981ecb
vhugopro <vitorhgfranca@gmail.com>
2024-04-18 10:13:42 -0300

    Category

---
+++ src/main/java/com/visualize/project/repositories/CategoryRepository.java
@@ -0,0 +1,10 @@
+package com.visualize.project.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.visualize.project.entities.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long>{
+
+	
+}
